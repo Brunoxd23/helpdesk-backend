b0c9dd61c588494a4f74f76a014a863e41c2a3c8
Brunoxd23 <monteiro_100@outlook.pt>
2023-07-26 15:05:35 -0300

    perfil de teste criado

---
+++ src/main/java/com/bruno/helpdesk/config/TestConfig.java
@@ -0,0 +1,21 @@
+package com.bruno.helpdesk.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.bruno.helpdesk.services.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+	
+	@Autowired DBService dbService;
+	
+	@Bean
+	public void instanciaDB() {
+		this.dbService.instanciaDB();
+	}
+	
+}
