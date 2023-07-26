869fc8f4d985568eeb5c07248dda1e303b206fb0
Brunoxd23 <monteiro_100@outlook.pt>
2023-07-26 17:04:09 -0300

    Protegendo aplicação contra serializacao

--- src/main/java/com/bruno/helpdesk/domain/Cliente.java
+++ src/main/java/com/bruno/helpdesk/domain/Cliente.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.bruno.helpdesk.domain.enums.Perfil;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
@@ -12,6 +13,7 @@ import jakarta.persistence.OneToMany;
 public class Cliente extends Pessoa {
 	private static final long serialVersionUID = 1L;
 	
+	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Chamado> chamados = new ArrayList<>();
 
