e58f03a7b7dfa154f4994570449f2da8423f0e5f
Brunoxd23 <monteiro_100@outlook.pt>
2023-07-26 09:36:42 -0300

    classe de dominio iniciadas

---
+++ src/main/java/com/bruno/helpdesk/domain/Chamado.java
@@ -0,0 +1,129 @@
+package com.bruno.helpdesk.domain;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+import com.bruno.helpdesk.domain.enums.Prioridade;
+import com.bruno.helpdesk.domain.enums.Status;
+
+public class Chamado {
+
+	private Integer id;
+	private LocalDate dataAbertura = LocalDate.now();
+	private LocalDate dataFechamento;
+	private Prioridade prioridade;
+	private Status status;
+	private String tirulo;
+	private String observacoes;
+	
+	private Tecnico tecnico;
+	private Cliente cliente;
+	
+	public Chamado() {
+		super();
+	}
+
+	public Chamado(Integer id, Prioridade prioridade, Status status, String tirulo, String observacoes, Tecnico tecnico,
+			Cliente cliente) {
+		super();
+		this.id = id;
+		this.prioridade = prioridade;
+		this.status = status;
+		this.tirulo = tirulo;
+		this.observacoes = observacoes;
+		this.tecnico = tecnico;
+		this.cliente = cliente;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public LocalDate getDataAbertura() {
+		return dataAbertura;
+	}
+
+	public void setDataAbertura(LocalDate dataAbertura) {
+		this.dataAbertura = dataAbertura;
+	}
+
+	public LocalDate getDataFechamento() {
+		return dataFechamento;
+	}
+
+	public void setDataFechamento(LocalDate dataFechamento) {
+		this.dataFechamento = dataFechamento;
+	}
+
+	public Prioridade getPrioridade() {
+		return prioridade;
+	}
+
+	public void setPrioridade(Prioridade prioridade) {
+		this.prioridade = prioridade;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public String getTirulo() {
+		return tirulo;
+	}
+
+	public void setTirulo(String tirulo) {
+		this.tirulo = tirulo;
+	}
+
+	public String getObservacoes() {
+		return observacoes;
+	}
+
+	public void setObservacoes(String observacoes) {
+		this.observacoes = observacoes;
+	}
+
+	public Tecnico getTecnico() {
+		return tecnico;
+	}
+
+	public void setTecnico(Tecnico tecnico) {
+		this.tecnico = tecnico;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Chamado other = (Chamado) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+}
