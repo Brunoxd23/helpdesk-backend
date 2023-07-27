f2a2b46dd000aa8cc0cd2ccd78745ff5a2c196c9
Brunoxd23 <monteiro_100@outlook.pt>
2023-07-27 15:28:05 -0300

    Exceção para objeto não encontrado

--- src/main/java/com/bruno/helpdesk/services/TecnicoService.java
+++ src/main/java/com/bruno/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.bruno.helpdesk.domain.Tecnico;
 import com.bruno.helpdesk.repositories.TecnicoRepository;
+import com.bruno.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class TecnicoService {
@@ -16,6 +17,6 @@ public class TecnicoService {
 
 	public Tecnico finById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não Encontrado! Id: " + id) );
 	}
 }
