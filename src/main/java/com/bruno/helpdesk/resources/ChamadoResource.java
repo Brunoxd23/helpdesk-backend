7f11b4c7919a7565c3450944c6cc2a5697c35593
Brunoxd23 <monteiro_100@outlook.pt>
2023-08-04 09:24:45 -0300

    update para chamado implementado

--- src/main/java/com/bruno/helpdesk/resources/ChamadoResource.java
+++ src/main/java/com/bruno/helpdesk/resources/ChamadoResource.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,8 +44,13 @@ public class ChamadoResource {
 	@PostMapping
 	public ResponseEntity<ChamadoDTO> create(@Valid @RequestBody ChamadoDTO objDTO) {
 		Chamado obj = service.create(objDTO);
-		URI uri = ServletUriComponentsBuilder.
-				fromCurrentRequestUri().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ChamadoDTO> update (@PathVariable Integer id, @Valid @RequestBody ChamadoDTO objDTO) {
+		Chamado newObj = service.update(id, objDTO);
+		return ResponseEntity.ok().body(new ChamadoDTO(newObj));
 	}
 }
--- src/main/java/com/bruno/helpdesk/services/ChamadoService.java
+++ src/main/java/com/bruno/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.bruno.helpdesk.services;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -40,6 +41,13 @@ public class ChamadoService {
 		return repository.save(newChamado(objDTO));
 	}
 
+	public Chamado update(Integer id, @Valid ChamadoDTO objDTO) {
+		objDTO.setId(id);
+		Chamado oldObj = findById(id);
+		oldObj = newChamado(objDTO);
+		return repository.save(oldObj);
+	}
+
 	private Chamado newChamado(ChamadoDTO obj) {
 		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
 		Cliente cliente = clienteService.findById(obj.getCliente());
@@ -47,6 +55,10 @@ public class ChamadoService {
 		Chamado chamado = new Chamado();
 		if (obj.getId() != null) {
 			chamado.setId(obj.getId());
+		}
+		
+		if(obj.getStatus().equals(2)) {
+			chamado.setDataFechamento(LocalDate.now());
 		}
 
 		chamado.setTecnico(tecnico);
